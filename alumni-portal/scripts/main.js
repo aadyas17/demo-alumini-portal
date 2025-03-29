70f5fa6d062137cfff73779e518d857b0c71c0fe
Aadya Shrivastava <aadya17shrivastava@gmail.com>
2025-03-29 13:36:55 +0530

    first commit

---
+++ alumni-portal/scripts/main.js
@@ -0,0 +1,11 @@
+function requestMentorship(mentorName) {
+    alert(`Mentorship request sent to ${mentorName}!`);
+}
+
+function loginUser(userType) {
+    alert(`Logging in as ${userType}...`);
+}
+
+function submitProfile(userType) {
+    alert(`${userType} profile updated successfully!`);
+}
