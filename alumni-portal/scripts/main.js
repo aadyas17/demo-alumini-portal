e728eb2540b6d23ef5f3caddecf1994a0d48fe07
Aadya Shrivastava <aadya17shrivastava@gmail.com>
2025-03-29 17:22:42 +0530

    added files

--- alumni-portal/scripts/main.js
+++ alumni-portal/scripts/main.js
@@ -1,20 +1,20 @@
-async function loginUser() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+// async function loginUser() {
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
 
-    // Sending login request to backend
-    const response = await fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
-    });
+//     // Sending login request to backend
+//     const response = await fetch('http://localhost:3000/login', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ email, password }),
+//     });
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    if (response.ok) {
-        alert('Login successful! Redirecting to dashboard...');
-        window.location.href = 'alumni-portal/dashboard/alumini-dashboard.html';  // Redirect
-    } else {
-        alert(data.error); // Show error message
-    }
-}
+//     if (response.ok) {
+//         alert('Login successful! Redirecting to dashboard...');
+//         window.location.href = 'alumni-portal/dashboard/alumini-dashboard.html';  // Redirect
+//     } else {
+//         alert(data.error); // Show error message
+//     }
+// }
